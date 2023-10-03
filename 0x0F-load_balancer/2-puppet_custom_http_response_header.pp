1b0f4c6b7b81382cc567a462d7a4076f9c0ea8c1
kintokeanu <brianislevu@gmail.com>
2023-10-03 05:41:25 +0300

    setting up ubuntu with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,28 @@
+# setup ubuntu server with nginx and add custom http header
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+    ensure => 'installed',
+    require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+    content => 'Hello World!'
+}
+
+exec {'redirect_me':
+    command => 'sed -i "24i\  rewrite ^/redirect_me https://mblevu.tech/ permanent;" /etc/nginx/sites-available/default',
+    provider => 'shell'
+}
+
+exec {'HTTP header':
+    command => 'sed -i "25i\  add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+    provider => 'shell'
+}
+
+service {'nginx:
+    ensure => running,
+    require => Package['nginx]
+}

f0bdbb27dcc4e255207f8bb696e0d0282e98e609
mblevu <brianislevu@gmail.com>
2023-11-18 13:45:16 +0300

    fixing apache

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,22 @@
+# Install Apache
+package { 'apache2':
+    ensure => 'installed',
+}
+
+# Configure Apache
+file { '/etc/apache2/mods-available/mpm_prefork.conf':
+    ensure  => 'file',
+    content => "StartServers             5\n" +
+                         "MinSpareServers          5\n" +
+                         "MaxSpareServers          10\n" +
+                         "MaxRequestWorkers        100\n" +
+                         "MaxConnectionsPerChild   0\n",
+    notify  => Service['apache2'],
+}
+
+# Enable and start Apache service
+service { 'apache2':
+    ensure    => 'running',
+    enable    => true,
+    subscribe => File['/etc/apache2/mods-available/mpm_prefork.conf'],
+}

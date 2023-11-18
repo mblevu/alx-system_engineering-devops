5bdbf0750b16308b9444bfb2f5f55023668634e6
mblevu <brianislevu@gmail.com>
2023-11-18 13:52:27 +0300

    fixing linting

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -11,7 +11,7 @@ file { '/etc/apache2/mods-available/mpm_prefork.conf':
                           "MinSpareServers          5\n" +
                           "MaxSpareServers          10\n" +
                           "MaxRequestWorkers        100\n" +
-                         "MaxConnectionsPerChild   0\n",
+                          "MaxConnectionsPerChild   0\n",
     notify  => Service['apache2'],
 }
 

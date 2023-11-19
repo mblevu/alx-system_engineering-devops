20044c65333843a10e796446f99bc49affcf0a50
mblevu <brianislevu@gmail.com>
2023-11-19 04:47:44 +0300

    modding user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# allows user holberton to login and open a file without any error message.
+exec { 'set_file_limit':
+  command => 'ulimit -n 65535',
+  path    => ['/bin', '/usr/bin'],
+  user    => 'holberton',
+  environment => 'HOME=/home/holberton',
+  onlyif  => 'test $(ulimit -n) -lt 65535',
+}

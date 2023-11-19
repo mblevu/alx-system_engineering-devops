8734d04c3139f2b8ef4445115c4a0c760fc5326c
mblevu <brianislevu@gmail.com>
2023-11-19 04:53:12 +0300

    modding user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,8 @@
 # allows user holberton to login and open a file without any error message.
-# Ensure user 'holberton' exists
-user { 'holberton':
-  ensure     => 'present',
-  managehome => true,
-  shell      => '/bin/bash',
-}
-
-# Ensure 'holberton' has access to a specific file
-file { '/path/to/your/file':
-  ensure => 'file',
-  owner  => 'holberton',
-  mode   => '0644',
+exec { 'set_file_limit':
+  command     => 'ulimit -n 65535',
+  path        => ['/bin', '/usr/bin'],
+  user        => 'holberton',
+  environment => 'HOME=/home/holberton',
+  onlyif      => 'test "$(ulimit -n)" -lt 65535',
 }
